2b6043ee4b0bf3699278f0f57339552c319042ad
Trinh Huy <giahuy050201@gmail.com>
2021-12-29 00:45:38 +0200

    Solution to chap10 ex

--- Chap10-FunctionsAndTasks/exSolution.v
+++ Chap10-FunctionsAndTasks/exSolution.v
@@ -1,7 +1,24 @@
-// Method 1: Procedural assignment
-always @*
-  y = (add==1) ? a+b : c;
-
-// Method 2: Continuous assignment
-   assign y = (add=1) ? a+b : c;
-   
+/*---My sol
+task increaseBus; 
+ input [31:0] abus;
+ integer sel;
+ sel <= 1;
+ always @(posedge clk )
+  begin
+  for (sel = 0; sel < 4; sel = sel + 1)
+   abus <= abus + 1;
+  end
+  sel <= 0;
+endtask
+*/
+task increaseBus; 
+ input [31:0] addr;
+ integer sel;
+ begin @(posedge clk )
+  sel <= 1;
+  abus <= addr;
+  repeat(3) @(posedge clk)
+   abus <= abus + 1;
+  sel <= 0;
+ end
+endtask
